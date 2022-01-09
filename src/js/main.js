a3e10ae86db7a75ffce6be8fecb5d69e2c13d351
Яковлев Александр <59922930+Alexandr-web@users.noreply.github.com>
2022-01-09 17:38:11 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,1 @@
 import Lizardx from "./Lizardx";
-<<<<<<< HEAD
-=======
-
-const { list, liz } = new Lizardx();
-
-const newArray = liz([1,2,3,4]).each(item => item += 2);
-
-console.log(newArray);
->>>>>>> b651b662ae80bf726d0cf3fc7f38e7a81bc4ffa1
